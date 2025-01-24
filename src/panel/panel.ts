d121875627e8f4584edcdc0573f2fb55bedbed5a
Tom Bruno <tom.bruno@smartqasa.com>
2025-01-24 13:57:54 -0500

    Set static background image for PanelCard and update build timestamp in console log

--- src/panel/panel.ts
+++ src/panel/panel.ts
@@ -135,12 +135,16 @@ export class PanelCard extends LitElement implements LovelaceCard {
         const name = this._config?.name ?? this._areaObj?.name ?? 'Area';
         const picture = this._config.picture ?? `${this._area}.png`;
 
+        /*
         const baseUrl = new URL(location.href).origin;
         const panelImage = getComputedStyle(document.documentElement)
             .getPropertyValue('--sq-panel-image')
             .trim();
         console.log('Background URL: ', `url(${baseUrl}${panelImage})`);
         this.style.backgroundImage = `url(${baseUrl}${panelImage})`;
+        */
+        this.style.backgroundImage =
+            'url("http://192.168.75.10:10075/local/smartqasa/backgrounds/dark.jpg")';
 
         return html`
             <div class="panel" ?admin=${this._isAdminMode}>
