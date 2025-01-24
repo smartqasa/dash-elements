a2a477dfa122dcae2a5c56217f0c9a56e8064d4b
Tom Bruno <tom.bruno@smartqasa.com>
2025-01-24 14:08:33 -0500

    Fix background image URL formatting in PanelCard and update build timestamp in console log

--- src/panel/panel.ts
+++ src/panel/panel.ts
@@ -139,8 +139,13 @@ export class PanelCard extends LitElement implements LovelaceCard {
         const panelImage = getComputedStyle(document.documentElement)
             .getPropertyValue('--sq-panel-image')
             .trim();
-        console.log('Background URL: ', `url(${baseUrl}${panelImage})`);
+        console.log('Background URL: ', `url("${baseUrl}${panelImage}")`);
         this.style.backgroundImage = `url("${baseUrl}${panelImage}")`;
+
+        /*
+        this.style.backgroundImage =
+            'url("http://192.168.75.10:10075/local/smartqasa/backgrounds/dark.jpg")';
+        */
 
         return html`
             <div class="panel" ?admin=${this._isAdminMode}>
